544004f45119f0c5608cb3388f4e3a41a1abcb8b
catcosmicice <catcosmicice@protonmail.ch>
2021-03-06 15:03:56 +0400

    h

---
+++ components/Header.vue
@@ -0,0 +1,141 @@
+<template>
+  <nav>
+    <input id="nav-toggle" type="checkbox">
+    <div class="logo">cosmicice</div>
+    <ul class="links">
+      <li><a class="link" href="#home">Home</a></li>
+      <li><a class="link" href="#about">About</a></li>
+      <li><a class="link" href="#work">Work</a></li>
+      <li><a class="link" href="#projects">Projects</a></li>
+      <li><a class="link" href="#contact">Contact</a></li>
+    </ul>
+    <label for="nav-toggle" class="icon-burger">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </label>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  nav {
+    @apply fixed left-0 right-0 top-0;
+    padding: 0 5%;
+    height: 100px;
+    background-color: transparent;
+    z-index: 10;
+  }
+
+  nav .logo {
+    @apply float-left flex items-center; 
+    width: 40%;
+    height: 100%;
+    font-size: 24px;
+    color: #282828;
+  }
+
+  nav .links {
+    @apply float-right p-0 m-0 flex justify-around items-center;
+    width: 60%;
+    height: 100%;
+  }
+
+  nav .links li {
+    @apply list-none uppercase;
+  }
+
+  nav .links a {
+    @apply block font-bold no-underline;
+    color: #282828;
+    font-size: 1.4em;
+  }
+
+  #nav-toggle {
+    @apply absolute;
+    top: -100px;
+  }
+
+  nav .icon-burger {
+    @apply hidden absolute cursor-pointer;
+    right: 5%;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  nav .icon-burger .line {
+    width: 30px;
+    height: 5px;
+    background-color: #f1f1f1;
+    margin: 5px;
+    border-radius: 3px;
+    transition: all .3s ease-in-out;
+  }
+
+  .link {
+    display: inline-block;
+    color: #000;
+    text-decoration: none;
+  }
+
+  .link::after {
+      content: '';
+      display: block;
+      width: 0;
+      height: 2px;
+      background: #000;
+      transition: width .3s;
+  }
+
+  .link:hover::after {
+      width: 100%;
+  }
+
+  @media screen and (max-width: 768px) {
+    nav .logo {
+      @apply float-none w-auto justify-center;
+    }
+
+    nav .links {
+      @apply float-none fixed w-auto h-auto flex-col justify-evenly overflow-hidden box-border;
+      left: 0;
+      right: 0;
+      bottom: 100%;
+      top: 100px;
+      background-color: rgba(0,0,0,.8);
+      color: #f1f1f1;
+      z-index: 9;
+      transition: all .5s ease-in-out;
+    }
+
+    nav .links a {
+      font-size: 1.7em;
+      color: #f1fff1;
+    }
+
+    nav :checked ~ .links {
+      bottom: 0;
+    }
+
+    nav .icon-burger {
+      @apply block;
+    }
+
+    nav :checked ~ .icon-burger .line:nth-child(1) {
+      transform: translateY(10px) rotate(225deg);
+    }
+
+    nav :checked ~ .icon-burger .line:nth-child(3) {
+      transform: translateY(-10px) rotate(-225deg) ;
+    }
+
+    nav :checked ~ .icon-burger .line:nth-child(2) {
+      opacity: 0;
+    }
+  }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'cosmicice',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '@assets/style.css' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&display=swap' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  loading: {
+    continuous: true
+  },
+
+  // Server conf
+  server: {
+    host: '[::1]'
+  }
+}
